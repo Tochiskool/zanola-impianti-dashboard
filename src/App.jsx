30b3ce9077ad9d54b663643d3cfdaf1be5d72b84
AChristian N <31229894+Tochiskool@users.noreply.github.com>
2025-04-26 18:12:12 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Menu from "./components/Menu/Menu";
+import "./App.css";
+const App = () => {
+  return (
+    <div className='App'>
+      <Menu />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../../images/logo.png";
+import "./menu.css";
+const Menu = () => {
+  return (
+    <menu>
+      <img src={Logo} alt='Main logo' />
+    </menu>
+  );
+};
+
+export default Menu;

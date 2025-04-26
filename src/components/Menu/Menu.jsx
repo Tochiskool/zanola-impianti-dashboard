812b97c8c89bdcb44f5aee0ec5cb92303001464f
AChristian N <31229894+Tochiskool@users.noreply.github.com>
2025-04-26 21:02:02 +0200

    Menu developed

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,58 @@
-import React from "react";
-import Logo from "../../images/logo.png";
+import React, { useState } from "react";
+import Logo from "../../images/zanola.png";
+import {
+  FaDelicious,
+  FaShoppingCart,
+  FaWallet,
+  FaChartLine,
+  FaRegClock,
+  FaCog,
+  FaSignOutAlt,
+} from "react-icons/fa";
+
 import "./menu.css";
+
 const Menu = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const menuItems = [
+    { icon: FaDelicious },
+    { icon: FaShoppingCart },
+    { icon: FaWallet },
+    { icon: FaChartLine },
+    { icon: FaRegClock },
+    { icon: FaCog },
+  ];
+  const lastMenu = [{ icon: FaCog }, { icon: FaSignOutAlt }];
   return (
     <menu>
       <img src={Logo} alt='Main logo' />
+      <ul id='mainMenu'>
+        {menuItems.map((item, index) => (
+          <li
+            key={index}
+            className={activeIndex === index ? "active" : ""}
+            onClick={() => setActiveIndex(index)}
+          >
+            <a href='#'>
+              <item.icon />
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className='lastMenu'>
+        {lastMenu.map((item, index) => (
+          <li
+            key={index}
+            className={activeIndex === index + 100 ? "active" : ""}
+            onClick={() => setActiveIndex(index + 100)}
+          >
+            <a href='#'>
+              <item.icon />
+            </a>
+          </li>
+        ))}
+      </ul>
     </menu>
   );
 };

5a964134699c1233203da15e8e9ee88348e908dd
AChristian N <31229894+Tochiskool@users.noreply.github.com>
2025-04-28 20:43:28 +0200

    top container completed

--- src/components/TopContainer/TopContainer.jsx
+++ src/components/TopContainer/TopContainer.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import { FaBell, FaChevronDown } from "react-icons/fa";
 const TopContainer = () => {
+  useEffect(() => {
+    const menuTarget = document.querySelector("#menuChevron");
+    const menuContainer = document.querySelector("#menuContainer");
+    menuTarget.addEventListener("mouseenter", () => {
+      menuTarget.style.transform = "rotate(180deg)";
+      menuContainer.style.transform = "translateX(0px)";
+    });
+    menuContainer.addEventListener("mouseleave", () => {
+      menuTarget.style.transform = "rotate(0deg)";
+      menuContainer.style.transform = "translateX(300px)";
+    });
+  }, []);
   return (
     <div className='topContainer'>
       <div className='inputBox'>
@@ -14,11 +26,22 @@ const TopContainer = () => {
         <i className='profileIcon'>
           <FaBell />
         </i>
-        <div className='profileImage'></div>
-        <p className='profileName'>Nicolas Zanola</p>
+        <div className='profileImage'>
+          <img src='/images/women.jpg' alt='Woman in sun shades' />
+        </div>
+        <p className='profileName'>Zanola</p>
         <i className='menuChevron' id='menuChevron'>
           <FaChevronDown />
         </i>
+        <div className='menuContainer' id='menuContainer'>
+          <ul>
+            <li>Inventory</li>
+            <li>Orders</li>
+            <li>hours covered</li>
+            <li>Supervisor</li>
+            <li>Complains</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
